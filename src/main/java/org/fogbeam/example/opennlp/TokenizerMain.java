9f2a267e78b77fa0242c38d62fea19971fa94544
Sergio Mrtin <sergio.ms.91@gmail.com>
2017-05-22 18:12:24 +0100

    Created Read Method

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -2,15 +2,43 @@
 package org.fogbeam.example.opennlp;
 
 import org.fogbeam.example.opennlp.AuxTokenizer;
+import java.io.*;
 
-public class TokenizerMain
-{
-	public static void main( String[] args ) throws Exception
-	{
+public class TokenizerMain {
+    public static void main(String[] args) throws Exception {
 
-		AuxTokenizer.tokenize
-				(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, "
-						+ " mentions \"three Mounts raised by the Indians over three of their Great Kings"
-						+ " who were killed in the Wars.\"" );
-	}
+        if (args.length > 0) {
+            for (final String filePath : args) {
+                //Read From File ==> RFF
+                AuxTokenizer.tokenize(RFF(filePath));
+            }
+        } else {
+            AuxTokenizer.tokenize("A ranger journeying with Oglethorpe, founder of the Georgia Colony, "
+                    + " mentions \"three Mounts raised by the Indians over three of their Great Kings"
+                    + " who were killed in the Wars.\"");
+        }
+    }
+    private static String RFF(final String filePath) {
+
+        String Content = "";
+        try {
+                final InputStream input = new FileInputStream(filePath);
+                final BufferedReader buff = new BufferedReader(new InputStreamReader(input));
+
+                String line = buff.readLine();
+                final StringBuilder stringBuilder = new StringBuilder();
+                    while (line != null) {
+                        stringBuilder.append(line).append("\n");
+                        line = buff.readLine();
+                    }
+                Content = stringBuilder.toString();
+            } catch (final FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (final IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+        return Content;
+    }
+
 }
